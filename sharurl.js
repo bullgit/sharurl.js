54cf0bd7c76a87e1f287963d075f71c6924a2399
José Pino <fraphcore@gmail.com>
2014-04-17 22:40:04 -0500

    update v1.2

--- sharurl.js
+++ sharurl.js
@@ -1,59 +1,64 @@
-/* 
-*
-* sharurl.js
-* v1.1
-* Design by Bullgit and legendary flyers for bullgit ...
-* Track the number of times your page has been shared on several social networks.
-* For bullgit.
-* Developer: @mrjopino
-*
-*/
-(function() {
-  var url, SharURL, conURL;
+/**
+ *
+ * sharurl.js
+ *
+ * Track the number of times your page has been shared on several social networks.
+ * Version: 1.2
+ *
+ * Find the project on GitHub: 
+ * https://github.com/mrjopino/sharurl.js
+ *
+ * ============================
+ * Copyright, 2014 by Jose Pino
+ * https://twitter.com/mrjopino
+ * ============================
+ */
+$(document).ready(function () { 
+  facebook_sharurl  = $('#sharFB');
+  twitter_sharurl   = $('#sharTW');
+  linkedin_sharurl  = $('#sharLI');
+  bufferapp_sharurl = $('#sharBA');
+  _callback_sharurl = '&callback=?';
 
-  url = "http://twitter.com"; // Substitute its URL
+  var sharurl_id, conURL;
 
-  $.getJSON("http://graph.facebook.com/" + url, function(json) {
-    return SharURL($("#conteoFB"), json.shares);
-});
+  $.getJSON("https://api.facebook.com/method/fql.query?query=select%20total_count%20from%20link_stat%20where%20url='" + _sharurl + "'&format=json", function(json) {
+    return sharurl_id($(facebook_sharurl), json[0].total_count);
+ });
 
-  $.getJSON("http://urls.api.twitter.com/1/urls/count.json?url=" + url + "&callback=?", function(json) {
-    return SharURL($("#conteoTW"), json.count);
-});
+  $.getJSON("http://urls.api.twitter.com/1/urls/count.json?url=" + _sharurl + _callback_sharurl, function(json) {
+    return sharurl_id($(twitter_sharurl), json.count);
+ });
 
-  $.getJSON("http://www.linkedin.com/countserv/count/share?url=" + url + "&callback=?", function(json) {
-    return SharURL($("#conteoLI"), json.count);
-});
+  $.getJSON("http://www.linkedin.com/countserv/count/share?url=" + _sharurl + _callback_sharurl, function(json) {
+    return sharurl_id($(linkedin_sharurl), json.count);
+ });
   
-  $.getJSON("https://api.bufferapp.com/1/links/shares.json?url=" + url + "&callback=?", function(json) {
-    return SharURL($("#conteoBA"), json.shares);
-});
+  $.getJSON("https://api.bufferapp.com/1/links/shares.json?url=" + _sharurl + _callback_sharurl, function(json) {
+    return sharurl_id($(bufferapp_sharurl), json.shares);
+ });
 
-  $.getJSON("http://api.pinterest.com/v1/urls/count.json?url=" + url + "&callback=?", function(json) {
-    return SharURL($("#conteoPT"), json.count);
-});
-
-  conURL = function($item) {
+  conURL = function($_url_conURL) {
     return setTimeout(function() {
-      var timeline, enlace, target;
-      timeline = $item.attr("sharurl") * 1;
-      target = $item.attr("data-sharurl") * 1;
-      enlace = timeline + Math.ceil((target - timeline) / 2);
-      $item.attr("sharurl", enlace);
-      $item.html(enlace);
-      if (enlace < target) {
-        return conURL($item);
+      var tl_shar, shar_enlace, target;
+      tl_shar = $_url_conURL.attr("sharurl") * 1;
+      target = $_url_conURL.attr("data-sharurl") * 1;
+      shar_enlace = tl_shar + Math.ceil((target - tl_shar) / 3); //Timeline
+      $_url_conURL.attr("sharurl", shar_enlace);
+      $_url_conURL.html(shar_enlace);
+      if (shar_enlace < target) {
+        return conURL($_url_conURL);
       }
     }, 100);
   };
 
-  SharURL = function($item, contar) {
-    if (contar == null) {
-      contar = null;
+  sharurl_id = function($_url_conURL, conteo_sharurl) {
+    if (conteo_sharurl == null) {
+      conteo_sharurl = null;
     }
-    $item.attr("data-sharurl", contar);
-    $item.attr("sharurl", 0);
-    return conURL($item);
+    $_url_conURL.attr("data-sharurl", conteo_sharurl);
+    $_url_conURL.attr("sharurl", 0);
+    return conURL($_url_conURL);
   };
 
-}).call(this);
+});
